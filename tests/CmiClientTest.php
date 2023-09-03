b87f58b4d607af25385e8f00a1ce40b636a1381b
ismaail <contact@ismaail.com>
2023-09-03 01:19:23 +0100

    Add CmiPageTest & CmiPaymentTest
    Add CmiPageTest & CmiPaymentTest
    
    Update test

--- tests/CmiClientTest.php
+++ tests/CmiClientTest.php
@@ -2,50 +2,35 @@
 
 declare(strict_types=1);
 
-use Mehdirochdi\CMI\CmiClient;
-use Mehdirochdi\CMI\Exception\InvalidArgumentException;
-
-test('test it require storekey', function () {
-    expect(fn() => new CmiClient())
-        ->toThrow(InvalidArgumentException::class, 'storekey is required');
-});
-
-test('test storekey null', function () {
-    expect(fn() => new CmiClient(['storekey' => null]))
-        ->toThrow(InvalidArgumentException::class, 'storekey is required');
-});
-
-test('test storekey has space', function () {
-    expect(fn() => new CmiClient(['storekey' => '123 256']))
-        ->toThrow(InvalidArgumentException::class, 'storekey cannot contain whitespace');
-});
+use CMI\CmiClient;
+use CMI\CmiPayment;
 
 test('test if hash is validated', function () {
-    $base_url = 'http://cmi-php.local/example';
-    $client = new Mehdirochdi\CMI\CmiClient([
-        'storekey' => '987456', // STOREKEY
-        'clientid' => '1234567', // CLIENTID
-        'oid' => '137ABC', // COMMAND ID IT MUST BE UNIQUE
-        'shopurl' => $base_url, // SHOP URL FOR REDIRECTION
-        'okUrl' => "$base_url/okFail.php", // REDIRECTION AFTER SUCCEFFUL PAYMENT
-        'failUrl' => "$base_url/okFail.php", // REDIRECTION AFTER FAILED PAYMENT
-        'email' => 'mehdi.rochdi@gmail.com', // YOUR EMAIL APPEAR IN CMI PLATEFORM
-        'BillToName' => 'mehdi rochdi', // YOUR NAME APPEAR IN CMI PLATEFORM
-        'BillToCompany' => 'company name', // YOUR COMPANY NAME APPEAR IN CMI PLATEFORM
-        'BillToStreet12' => '100 rue adress', // YOUR ADDRESS APPEAR IN CMI PLATEFORM NOT REQUIRED
-        'BillToCity' => 'casablanca', // YOUR CITY APPEAR IN CMI PLATEFORM NOT REQUIRED
-        'BillToStateProv' => 'Maarif Casablanca', // YOUR STATE APPEAR IN CMI PLATEFORM NOT REQUIRED
-        'BillToPostalCode' => '20230', // YOUR POSTAL CODE APPEAR IN CMI PLATEFORM NOT REQUIRED
-        'BillToCountry' => '504', // YOUR COUNTRY APPEAR IN CMI PLATEFORM NOT REQUIRED (504=MA)
-        'tel' => '0021201020304', // YOUR PHONE APPEAR IN CMI PLATEFORM NOT REQUIRED
-        'amount' => '10.60', // RETRIEVE AMOUNT WITH METHOD POST
-        'CallbackURL' => "$base_url/callback.php", // CALLBACK
+    $baseUrl = 'http://cmi-php.local/example';
+    $client = new CmiClient(new CmiPayment([
+        'storekey' => '987456',
+        'clientid' => '1234567',
+        'oid' => '137ABC',
+        'shopurl' => $baseUrl,
+        'okUrl' => "$baseUrl/okFail.php",
+        'failUrl' => "$baseUrl/okFail.php",
+        'email' => 'mehdi.rochdi@gmail.com',
+        'BillToName' => 'mehdi rochdi',
+        'BillToCompany' => 'company name',
+        'BillToStreet12' => '100 rue adress',
+        'BillToCity' => 'casablanca',
+        'BillToStateProv' => 'Maarif Casablanca',
+        'BillToPostalCode' => '20230',
+        'BillToCountry' => '504',
+        'tel' => '0021201020304',
+        'amount' => '10.60',
+        'CallbackURL' => "$baseUrl/callback.php",
 
         'rnd' => '0.29594500 1693675195', // mock random microtime()
-    ]);
+    ]));
 
     $client->generateHash();
-    expect($client->Hash)
+    expect($client->generateHash())
         ->not()->toBeNull()
         ->toEqual('AyLnNPwao8EnbcrLo0AfAF5LGzRBfQfSpmeUIyhes+uUR8+DbhpbxT/JgXqTQYOcnpkB+kfD2rYZ5S8FpT08AQ==')
     ;
